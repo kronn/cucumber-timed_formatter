da234e6e0a8ece4045c77d8483390344c9201704
Matthias Viehweger <kronn@kronn.de>
2011-12-14 10:36:28 +0100

    add after_step_result to output error instantly

--- lib/timed.rb
+++ lib/timed.rb
@@ -17,6 +17,13 @@ module Cucumber
         @duration += Time.now - @my_time
       end
 
+      def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
+        super
+        if status == :failed
+          @io.puts step_match.backtrace_line
+        end
+      end
+
       def after_feature_element(scenario)
         @io.puts " #{scenario.name} (#{@duration}s)"
         @duration = 0.0
